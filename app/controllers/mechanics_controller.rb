54016f3a986c6e8b8c451d4accd85e11360ae17b
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-09-18 22:48:35 -0600

    Attempt at completing User Story 2

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -1,5 +1,11 @@
 class MechanicsController < ApplicationController
   def show
     @mechanic = Mechanic.find(params[:id])
+
+    if params[:ride_id].present?
+      ride = Ride.find(params[:ride_id])
+      @mechanic.rides << ride
+      redirect_to mechanic_path(@mechanic)
+    end
   end
 end
--- spec/features/mechanics/show_spec.rb
+++ spec/features/mechanics/show_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe "mechanic show page" do
       visit mechanic_path(mechanic2)
 
       within("#mechanic-show-#{mechanic2.id}") do
+        expect(page).to have_content(mechanic2.name)
         expect(page).to have_content(mechanic2.years_experience)
         expect(page).to have_content(ride1.name)
       end
@@ -29,7 +30,47 @@ RSpec.describe "mechanic show page" do
         expect(page).to have_content(mechanic4.name)
         expect(page).to have_content(mechanic4.years_experience)
         expect(page).to have_content(ride2.name)
-        save_and_open_page
+      end
+    end
+
+    it "then I see a form to add a ride to their workload & when I fill in that field with an id of an existing ride and click Submit, I’m taken back to that mechanic's show page and I see the name of that newly added ride on this mechanic's show page." do
+      amusement_park1 = create(:amusement_park)
+      
+      mechanic1 = create(:mechanic)
+      mechanic2 = create(:mechanic)
+      mechanic3 = create(:mechanic)
+      mechanic4 = create(:mechanic)
+
+      ride1 = create(:ride)
+      ride2 = create(:ride)
+
+      RideMechanic.create!(ride_id: ride1.id, mechanic_id: mechanic2.id)
+      RideMechanic.create!(ride_id: ride2.id, mechanic_id: mechanic4.id)
+
+      visit mechanic_path(mechanic2)
+
+      fill_in 'Ride ID', with: ride2
+      click_button 'Submit'
+
+      within("#mechanic-show-#{mechanic2.id}") do
+        expect(current_path).to eq(mechanic_path(mechanic2))
+        expect(page).to have_content(mechanic2.name)
+        expect(page).to have_content(mechanic2.years_experience)
+        expect(page).to have_content(ride1.name)
+        expect(page).to have_content(ride2.name)
+      end
+
+      visit mechanic_path(mechanic4)
+
+      fill_in 'Ride ID', with: ride1
+      click_button 'Submit'
+
+      within("#mechanic-show-#{mechanic4.id}") do
+        expect(current_path).to eq(mechanic_path(mechanic4))
+        expect(page).to have_content(mechanic4.name)
+        expect(page).to have_content(mechanic4.years_experience)
+        expect(page).to have_content(ride2.name)
+        expect(page).to have_content(ride1.name)
       end
     end
   end
